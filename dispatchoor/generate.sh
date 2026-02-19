4d22222afd44806f0156a88cb494ccf5ef442273
Rafael Matias <rafael@skyle.net>
2026-02-19 13:09:27 +0100

    increase timeouts for reth and erigon on compute tests

--- dispatchoor/generate.sh
+++ dispatchoor/generate.sh
@@ -27,6 +27,12 @@ for client in "${CLIENTS[@]}"; do
     for test_type in "${TEST_TYPES[@]}"; do
       test_type_display="$(tr '[:lower:]' '[:upper:]' <<< "${test_type:0:1}")${test_type:1}"
 
+      # 12h timeout for erigon/reth compute tests, 6h for everything else
+      timeout="360"
+      if [[ "$test_type" == "compute" && ("$client" == "erigon" || "$client" == "reth") ]]; then
+        timeout="720"
+      fi
+
       entries+=("- id: benchmarkoor-${client}-${slug}-${test_type}
   name: \"Benchmarkoor (${client_display}) - ${network_display}(${block}) - ${test_type_display}\"
   owner: ethpandaops
@@ -39,7 +45,7 @@ for client in "${CLIENTS[@]}"; do
     block: \"${block}\"
     test-type: \"${test_type}\"
   inputs:
-    run-timeout-minutes: \"360\"
+    run-timeout-minutes: \"${timeout}\"
     clients: '[\"${client}\"]'
     snapshot: \"${snapshot}\"
     test-type: \"${test_type}\"
