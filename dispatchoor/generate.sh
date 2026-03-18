9463a60916fb43fe1b14ba2a4c19e22ca12a4b27
Rafael Matias <rafael@skyle.net>
2026-03-18 13:05:39 +0100

    new timeouts

--- dispatchoor/generate.sh
+++ dispatchoor/generate.sh
@@ -28,10 +28,13 @@ for client in "${CLIENTS[@]}"; do
     for test_type in "${TEST_TYPES[@]}"; do
       test_type_display="$(tr '[:lower:]' '[:upper:]' <<< "${test_type:0:1}")${test_type:1}"
 
-      # 13h timeout for perf-devnet-3 and mainnet stateful runs,
+      # 26h timeout for perf-devnet-3 stateful runs,
+      # 13h timeout for mainnet stateful runs,
       # 12h timeout for erigon/reth compute tests, 6h for everything else
       timeout="360"
-      if [[ "$test_type" == "stateful" && ("$network" == "perf-devnet-3" || "$network" == "mainnet") ]]; then
+      if [[ "$test_type" == "stateful" && "$network" == "perf-devnet-3" ]]; then
+        timeout="1560"
+      elif [[ "$test_type" == "stateful" && "$network" == "mainnet" ]]; then
         timeout="780"
       elif [[ "$test_type" == "compute" && ("$client" == "erigon" || "$client" == "reth") ]]; then
         timeout="720"
